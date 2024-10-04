cd3301b9dd82a2d01c371e42acd55360d353decc
Nahid Khalili <Nahid_khalili@ymail.com>
2024-10-04 11:34:30 +0330

    Loader added

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,15 +1,17 @@
 import { useProducts } from "../context/ProductContext";
 import Card from "../components/Card";
 import styles from "./Products.module.css";
+import Loader from "../components/Loader";
 const Products = () => {
   const products = useProducts();
   console.log(products);
   return (
     <div className={styles.container}>
       <div className={styles.products}>
-        {!products.length && <p>Loading...</p>}
-        {!!products.length &&
-          products.map((product) => <Card key={product.id} data={product} />)}
+        {!products.length && <Loader />}
+        {products.map((product) => (
+          <Card key={product.id} data={product} />
+        ))}
       </div>
     </div>
   );
