ab46f887ded1193e1580288bfeb658478d60e842
Nahid Khalili <Nahid_khalili@ymail.com>
2024-10-07 23:02:50 +0330

    add category filter and search filtter for products

--- src/helper/helper.js
+++ src/helper/helper.js
@@ -3,4 +3,16 @@ const shortenText = (text) => {
   return shortText;
 };
 
-export {shortenText}
+const searchValue = (products, search) => {
+  if (!search) return products;
+  const result = products.filter((p) => p.title.toLowerCase().includes(search));
+  return result;
+};
+
+const categoryFilter = (products, category) => {
+  if (!category) return products;
+  const result = products.filter((p) => p.category == category);
+  return result;
+};
+
+export { shortenText, searchValue, categoryFilter };
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -2,18 +2,71 @@ import { useProducts } from "../context/ProductContext";
 import Card from "../components/Card";
 import styles from "./Products.module.css";
 import Loader from "../components/Loader";
+import { FaList } from "react-icons/fa";
+import { useEffect, useState } from "react";
+import { categoryFilter } from "../helper/helper";
+import { searchValue } from "../helper/helper";
+
 const Products = () => {
   const products = useProducts();
-  console.log(products);
+  const [showProducts, setShowProducts] = useState([]);
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState({});
+  console.log(showProducts);
+
+  useEffect(() => {
+    setShowProducts(products);
+  }, [products]);
+
+  useEffect(() => {
+    let finalResult = categoryFilter(products, query.category);
+    finalResult = searchValue(finalResult, query.search);
+    setShowProducts(finalResult);
+  }, [query]);
+
+  const categoryHandler = (event) => {
+    const category = event.target.innerText.toLowerCase();
+    if (event.target.tagName !== "LI") return;
+    setQuery((q) => ({ ...q, category }));
+  };
+
+  const searchHandler = () => {
+    setQuery((q) => ({ ...q, search }));
+  };
+
   return (
-    <div className={styles.container}>
-      <div className={styles.products}>
-        {!products.length && <Loader />}
-        {products.map((product) => (
-          <Card key={product.id} data={product} />
-        ))}
+    <>
+      <div>
+        <input
+          type="text"
+          placeholder="Search..."
+          value={search}
+          onChange={(e) => setSearch(e.target.value.toLowerCase().trim())}
+        />
+        <button onClick={searchHandler}>Click</button>
       </div>
-    </div>
+      <div className={styles.container}>
+        <div className={styles.products}>
+          {!showProducts.length && <Loader />}
+          {showProducts.map((product) => (
+            <Card key={product.id} data={product} />
+          ))}
+        </div>
+        <div>
+          <div>
+            <FaList />
+            Categories
+          </div>
+          <ul onClick={categoryHandler}>
+            <li>All</li>
+            <li>Electronics</li>
+            <li>Jewelery</li>
+            <li>Men's Clothing</li>
+            <li>Women's Clothing</li>
+          </ul>
+        </div>
+      </div>
+    </>
   );
 };
 
